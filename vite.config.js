8a779963ebc8182d3c5df0852002e643dbff8f18
monaa-rm <monaa.rm95@gmail.com>
2023-08-01 04:28:55 +0330

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/movito",
+  base: "/react-movito",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['js-big-decimal']
